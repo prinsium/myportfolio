10ef5988d0bf0e16a99b8f356e406b04a2ff74bd
Prince Trivedi <122072887+prinsium@users.noreply.github.com>
2023-10-07 11:07:38 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         id: 1,
         demolink: 'https://capy-baras.netlify.app',
-        sourcelink: 'https://github.com/3vdi/socialpost-frontend',
+        sourcelink: 'https://github.com/prinsium/socialpost-frontend',
         title: 'Social Media Web App - Capybaras',
         description: 'A fully functional social media web application where users can create, update, delete posts and comments and also chat with other users',
         technologies: 'ReactJs, MongoDB, ExpressJs, NodeJs, Material UI'
@@ -10,7 +10,7 @@ export const projects = [
     {
         id: 2,
         demolink: 'https://superprompts.netlify.app',
-        sourcelink: 'https://github.com/3vdi/superprompts',
+        sourcelink: 'https://github.com/prinsium/superprompts',
         title: 'Library of the best AI prompts - superprompts',
         description: 'Library of best AI prompts for midjourney, chatGPT, DALL-E, etc, where users can create, copy, update, delete and search prompts.',
         technologies: 'NextJs, MongoDB, ExpressJs'
@@ -18,15 +18,15 @@ export const projects = [
     {
         id: 3,
         demolink: 'https://cloudlogs.netlify.app',
-        sourcelink: 'https://github.com/3vdi/cloud-logs-frontend',
+        sourcelink: 'https://github.com/prinsium/cloud-logs-frontend',
         title: 'Online Notes Web App - Cloud-Logs',
         description: 'A full stack web application for saving your notes online where users can create, read, update and delete Notes.',
         technologies: 'ReactJs, MongoDB, ExpressJs, NodeJs, Material UI'
     },
     {
         id: 4,
-        demolink: 'https://github.com/3vdi/flipnews',
-        sourcelink: 'https://github.com/3vdi/flipnews',
+        demolink: 'https://github.com/prinsium/flipnews',
+        sourcelink: 'https://github.com/prinsium/flipnews',
         title: 'News Web App - Flipnews',
         description: 'A fully responsive news web application which uses newsAPI to deliver news from multiple websites',
         technologies: 'ReactJs, React-router, Bootstrap'
@@ -34,7 +34,7 @@ export const projects = [
     {
         id: 5,
         demolink: 'https://wordutility.netlify.app',
-        sourcelink: 'https://github.com/3vdi/Word-Utility',
+        sourcelink: 'https://github.com/prinsium/Word-Utility',
         title: 'Word-Utility',
         description: 'A text utility based web application',
         technologies: 'ReactJs, Bootstrap'
